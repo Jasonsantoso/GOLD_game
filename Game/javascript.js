05ca040b0c897783102eb01e2ce652edd870ef14
Jasonsantoso <santoso.jason13@gmail.com>
2014-12-23 12:04:00 +0700

    Update
    Tambahin button di page 1 buat pilihan boy / girl

--- Game/javascript.js
+++ Game/javascript.js
@@ -86,5 +86,14 @@ function waktu() {
 	document.getElementById("date").innerHTML = tanggalText;
 	time = setTimeout(function(){waktu()}, 500);
 }
-
 waktu();
+
+function pilihboy() {
+	cwo = document.getElementById("boy").value;
+	document.getElementById("play_as").innerHTML = cwo;
+}
+
+function pilihgirl() {
+	cwe = document.getElementById("girl").value;
+	document.getElementById("play_as").innerHTML = cwe;
+}
